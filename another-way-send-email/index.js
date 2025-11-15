ba867b137cdf9d4f6d1d92e13004f042dcec8c2c
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-11-15 06:46:08 +0100

    send mail with resend

--- another-way-send-email/index.js
+++ another-way-send-email/index.js
@@ -1,15 +1,13 @@
-import express from 'express';
+import 'dotenv/config';
 import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 await resend.emails.send({
-    from: 'onboarding@resend.dev',
-    to: 'igscosmetics@gmail.com',
-    subject: 'Welcome',
-    html: '<strong>Thanks for signing up!!!</strong>'
-});
+        from: 'onboarding@resend.dev',
+        to: 'igscosmetics@gmail.com',
+        subject: 'Welcome',
+        html: '<strong>Thanks for signing up!!!</strong>'
+    });
 
-
-const port = 3000;
-app.listen(port, () => console.log(`Server is listening on port: ${port}`));
+  
