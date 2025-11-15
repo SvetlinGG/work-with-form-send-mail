8e18a3b22927d30c90ac91641e303d0c866d0b63
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-11-15 06:36:27 +0100

    message structure

--- another-way-send-email/index.js
+++ another-way-send-email/index.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import { Resend } from 'resend';
 
+const resend = new Resend(process.env.RESEND_API_KEY);
 
-const app = express();
+await resend.emails.send({
+    from: 'onboarding@resend.dev',
+    to: 'igscosmetics@gmail.com',
+    subject: 'Welcome',
+    html: '<strong>Thanks for signing up!!!</strong>'
+});
 
 
-
-const port = 5000;
+const port = 3000;
 app.listen(port, () => console.log(`Server is listening on port: ${port}`));
